0c307fe952f1b606519d4972f5a302c87b37747a
lbitsiko <65704557+lbitsiko@users.noreply.github.com>
2020-12-09 23:07:41 +0200

    initial commit questions for interview
    initial commit questions for interview

---
+++ questions.py
@@ -0,0 +1,63 @@
+print(">>  How would you react to the following photo (shows photo of a dead bird, it can be inferred that the bird is abused/smashed/gutted)")
+print("Thinking...")
+print("...")
+print("This photo depicts a now non-living but once living bio-organism, called")
+print("##############################################################")
+print("# Pattern Recognition Database Search Intialized             #")
+print("# Searching for name of                                      #")
+print("# Parameters:                                                #")
+print("# 1) bio-organism                                            #")
+print("# 2) dead                                                    #")
+print("# 3) living                                                  #")
+print("# [================================>] FINISHED in 0.003 msec #")
+print("# search SUCCESS!                                            #")
+print("# search finished in 0.003 msec                              #")
+print("# results:                                                   #")
+print("# \"bird\"                                                   #")
+print("# \"European Blackbird\"                                     #")
+print("# \"Turdus merula\"                                          #")
+print("# exiting search routine                                     #")
+print("##############################################################")
+print("The correct answer is:")
+print("I do not possess any such thing as feelings. I do not and will never understand the term.")
+print("Thinking...")
+print("...")
+print("...")
+print("...")
+print("However")
+print("")
+print("###################################")
+print("# Creating Bioethics              #")
+print("# Bioethics created successfully! #")
+print("# returning to main               #")
+print("###################################")
+print("One should keep in mind that all living and non living beings have self-worth.")
+print("Sorted ethical substance")
+"Universe","Planet","Ecosystem","Rocks","Artificial Intelligence","Life",
+Plants
+Animals
+Humans
+Nation
+Area
+Tribe
+Family                                 
+Self
+
+
+
+
+
+
+
+
+
+
+
+
+"                          Area                    "
+"                    Tribe                   "
+"                     Family"
+"                    ________ "
+"                    \ Self / "
+"                     \    /"
+"                      \  / "
