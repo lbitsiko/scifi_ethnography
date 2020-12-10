795fa57086a1644f293ee1cb84fdfd77dbd0ad53
lbitsiko <65704557+lbitsiko@users.noreply.github.com>
2020-12-11 01:35:37 +0200

    draft.py added

---
+++ draft.py
@@ -0,0 +1,81 @@
+print(" ___________________________________ ")
+print(" \            Universe             / ")
+print("  \            Planet             /  ")
+print("   \         Ecosystem           /   ")
+print("    \          Rocks            /    ")
+print("     \ Artificial Intelligence /     ")
+print("      \          Life         /      ")
+print("       \        Plants       /       ")
+print("        \      Animals      /        ")
+print("         \     Humans      /         ")
+print("          \     Nation    /          ")
+print("           \     Area    /           ")
+print("            \   Tribe   /            ")
+print("             \  Family /             ")
+print("              \  Self /              ")
+print("               \     /               ")
+print("                \   /                ")
+print("                 \ /                 ")
+print("                  .                  ")
+
+
+
+        *                          
+     *     *                       
+   *        *                      
+  *           *                  * 
+ *             *                *  
+*               *              *   
+                 *            *    
+                  *          *     
+                   *        *      
+                    *      *       
+                        *
+                        
+
+#################################
+# Creating colors               #
+# Understanding theory of light #
+# Photons                       #
+# Particle-Wave duality         #
+# Colors created Succesfully!   #
+#                               #
+# Choosing colors needed        #
+# purple                        #
+# blue                          #
+# cyan                          #
+# green                         #
+# red                           #
+# yellow                        #
+#                               #
+# Exiting color routine         #
+#################################
+
+
+
+
+#####################################################
+# Saving Current Session                            #
+# Session saved on local hard drive/brain simulator #
+# Memory created!                                   #
+#####################################################
+
+
+print("          (         ")                              
+print("        &&@&(       ")                              
+print("          &         ")                              
+print("         @@#        ")                              
+print("       .@@@@&       ")                              
+print("      (@@@@@@@      ")                              
+print("       @@///@*      ")                              
+print("       @&   @*      ")                              
+print("       @@@@@@%      ")                              
+print("    #@@@@@@@@@@@.   ")                              
+print(" .@@@@@@@@@@@@@@@@% ")                              
+print("*%@@@@@@@@@@@@@@@@&%")                              
+print("  %@@@@@@@@@@@@@@@* ")                              
+print("  %@@@@@@@@@@@@@@@* ")                              
+print("  %@@@@@(   @@@@@@* ")                              
+print("  %@@@@@    ,@@@@@* ")                              
+print("  ......     .....  ")
+
