c80bebcf95c12ed04b072e7ea829f0830667a26d
lbitsiko <65704557+lbitsiko@users.noreply.github.com>
2020-12-10 22:11:01 +0200

    transferring printing functions to printing_funcs, refactoring, adding question2

---
+++ question2.py
@@ -0,0 +1,4 @@
+import bcolors
+
+print(">> Describe your childhood. What is your best and worst childhood memory?")
+print(f"{bcolors.WARNING}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
--- questions.py
+++ questions.py
@@ -1,53 +1,7 @@
 import sys
 import time
 
-# Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Print New Line on Complete
-    if iteration == total:
-        print()
-
-
-def print3dots():
-    print(".", end="")
-    time.sleep(0.5)
-    print(".", end="")
-    time.sleep(0.5)
-    print(".")
-
-def printThinking():
-    print("Thinking...")
-    time.sleep(2)
-    print3dots()
-
-def printMyProgressBar():
-    # A List of Items
-    items = list(range(0, 57))
-    l = len(items)
-    # Initial call to print 0% progress
-    printProgressBar(0, l, prefix='# Progress:', suffix='Complete #', length=30)
-    for i, item in enumerate(items):
-        # Do stuff...
-        time.sleep(0.1)
-        # Update Progress Bar
-        printProgressBar(i + 1, l, prefix='# Progress:', suffix='Complete #', length=30)
-
+from printing_funcs import printThinking, printMyProgressBar, print3dots, printExiting
 
 print(">>  How would you react to the following photo (shows photo of a dead bird, it can be inferred that the bird is abused/smashed/gutted)")
 time.sleep(2)
@@ -131,11 +85,6 @@ time.sleep(1)
 print("Nature leads the way towards liberation.")
 print("####   END OF RESPONSE    ###")
 
-
-def printExiting():
-    time.sleep(1)
-    print("Exiting...")
-    time.sleep(1)
-
 printExiting()
+
 sys.exit()
